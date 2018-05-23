6d44e0d8612590767142519ec1f17d703db44769
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-05-23 15:42:39 -0500

    eighth

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+let id;
+let name;
     function post(){
         
         let Burger = {
@@ -9,6 +10,7 @@ $(document).ready(function(){
         
         $.post("/api/Burgers", Burger).then(function(data){
             //console.log(data)
+
             add(data);
         });
     };
@@ -16,7 +18,7 @@ $(document).ready(function(){
     function add(){
         $.get("/api/Burgers", function(data){
             if(data){
-                let div = $("<div>");
+                let div = $("<div id=" + data[data.length-1].id +">");
 
                 div.append("<h3>" + data[data.length-1].burger_name + "</h3><button class='js-devourButton' data-id=" + data[data.length-1].id +">'Devour Me!'</button>");
                 
@@ -24,12 +26,44 @@ $(document).ready(function(){
                 //console.log(data[data.length-1].burger_name)
                 $("#order-box").append(div);
             }
+        }).then(function(data){
+            $(".js-devourButton").on("click", function(event){
+
+                event.preventDefault();
+                //console.log(data);
+                id = $(this).data('id');
+                name = data[data.length-1].burger_name;
+                //console.log(id, name);
+               devour(id, name);
+            })
         })
+    };
+
+    function devour(){
+
+        $.ajax({
+            method: "PUT",
+            url: "/api/Burgers",
+            data: { id: id}
+        })
+        .then(function(data){
+            console.log(name)
+            //console.log(id);
+        $('#'+id).remove();
+        let div = $("<div>");
+
+        div.append("<h3>" + name + " 'has been eaten'</h3>");
+
+        $("#devoured-box").append(div);
+        })
+        
     };
 
     $("#submitButton").on("click", function(event){
         event.preventDefault();
         post();
     });
+
+
 
 })
