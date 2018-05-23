fcf615f8378d877b8763a3f69417e4409ece61e0
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-05-23 13:42:25 -0500

    sixth

--- public/js/script.js
+++ public/js/script.js
@@ -1,66 +1,35 @@
-$(function(){
-let dataId;
+$(document).ready(function(){
 
-function add(){
+    function post(){
         
-    let Burger = {
-        burger_name: $("#burger_name").val().trim(),
-        devoured: false
+        let Burger = {
+            burger_name: $("#burger_name").val().trim(),
+            devoured: false
+        };
+        
+        $.post("/api/Burgers", Burger).then(function(data){
+            //console.log(data)
+            add(data);
+        });
     };
-    
-    $.post("/api/Burgers", Burger).then(function(data){
-        show(data);
-    });
-};
 
-function devour(){
+    function add(){
+        $.get("/api/Burgers", function(data){
+            if(data){
+                let div = $("<div>");
 
-    $.ajax({
-        method: "PUT",
-        url: "/api/Burgers",
-        data: { id: dataId}
-    }).then(show());
-};
-
-function show(){
-    $.get("/api/Burgers", function(Burger){
-        if(Burger.length !== 0){
-            for (let i = 0; i < Burger.length; i++){
-                let div = $('<div>');
-
-                if(Burger[i].devoured === false){
-
-                    div.append("<h3>" + Burger[i].burger_name + "</h3><button class='js-devourButton' data-id=" + Burger[i].id +">'Devour it!'</button>");
-
-                    $("#order-box").append(div);
-                }
-                if(Burger[i].devoured === true){
-                    div.append("<h3>" + Burger[i].burger_name + " 'has been eaten'</h3>");
-
-                    $("#devoured-box").append(div);
-                }
-
+                div.append("<h3>" + data[data.length-1].burger_name + "</h3><button class='js-devourButton' data-id=" + data[data.length-1].id +">'Devour Me!'</button>");
+                
+                //console.log(data.length);
+                //console.log(data[data.length-1].burger_name)
+                $("#order-box").append(div);
             }
-        }
-    }).then(function(results){
-        
-        $(".js-devourButton").on("click", function(event){
-
-            event.preventDefault();
-            dataId = $(this).data('id');
-            console.log(dataId);
-
-            devour();
-            //.then(function(req, res){
-            //    show();
-           // });
         })
-    })
-};
+    };
 
-$("#submitButton").on("click", function(event){
-    event.preventDefault();
-    add();
-});
+    $("#submitButton").on("click", function(event){
+        event.preventDefault();
+        post();
+    });
 
 })
