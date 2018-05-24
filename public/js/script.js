163f0336df3d88933b8b36b6bc4c580485bd4801
Benjamin Miller <benjamin.miller2@northwestern.edu>
2018-05-24 11:23:10 -0500

    tenth

--- public/js/script.js
+++ public/js/script.js
@@ -1,89 +1,78 @@
 $(document).ready(function(){
-//let id;
-//let name;
-let Data;
 
+//required functions
+
+//function to post user input to the database
     function post(){
-        
+        //creates an object reflecting the DB's table
         let Burger = {
-            burger_name: $("#burger_name").val().trim(),
+            burger_name: $("#order").val().trim(),
             devoured: false
         };
         
+        //posts the data and fires the Add function
         $.post("/api/Burgers", Burger).then(function(data){
             //console.log(data)
-
             add(data);
         });
     };
 
+    //function to add new data from the DB to the dom.
     function add(){
         $.get("/api/Burgers", function(data){
+            
             if(data){
-                let div = $("<div id=" + data[data.length-1].id +">");
-
-                div.append("<h3>" + data[data.length-1].burger_name + "</h3><button class='js-devourButton' data-id=" + data[data.length-1].id +">'Devour Me!'</button>");
                 
-                //console.log(data.length);
-                //console.log(data[data.length-1].burger_name)
+                let burgerId = data[data.length-1].id;
+                let burgerName = data[data.length-1].burger_name;
+
+                //creates a new div
+                let div = $("<div class='burger' id=" + burgerId +">");
+                div.append("<p>Your </p><h3> " + burgerName + "</h3><p> is ready to eat!</p><button class='js-devourButton devourButton' data-id=" + burgerId +">'Devour Me!'</button>");
+                
+                //and appends it to the dom
                 $("#order-box").append(div);
-            }
+            };
+
         }).then(function(data){
+            
+            //This promise contains the functionality of the newly generated div buttons
+            //as well as the ajax call to update the relevant table date in the DB.
+            //Note, I originally attempted to include this in a seperate function, but was unable 
+            //to get the necessary data to pass into it. It would only work when attached to Add as a promise.
             $(".js-devourButton").on("click", function(event){
 
                 event.preventDefault();
-                //console.log(data);
                 
                 let id = $(this).data('id');
                 let name = data[id-1].burger_name;
-                console.log(id, name);
                 
+                //Call to update table data in the DB
                 $.ajax({
                     method: "PUT",
                     url: "/api/Burgers",
                     data: { id: id}
                 })
                 .then(function(data){
-                    //console.log(name)
-                    //console.log(id);
-                $('#'+id).remove();
-                let div = $("<div id=" + id +">");
-        
-                div.append("<h3>" + name + " 'has been eaten'</h3>");
-        
-                $("#devoured-box").append(div);
+
+                    $('#'+id).remove();
+
+                    let div = $("<div id=" + id +">");
+                    $(div).addClass("burger");
+                    div.append("<p>Your </p><h3>" + name + "</h3><p> has been devoured!</p>");
+                    
+                    $("#devoured-box").append(div);
                 })
 
             })
         })
     };
 
-    function devour(){
-            console.log(result);
-
-        $.ajax({
-            method: "PUT",
-            url: "/api/Burgers",
-            data: { id: id}
-        })
-        .then(function(data){
-            console.log(name)
-            //console.log(id);
-        $('#'+id).remove();
-        let div = $("<div id=" + id +">");
-
-        div.append("<h3>" + name + " 'has been eaten'</h3>");
-
-        $("#devoured-box").append(div);
-        })
-        
-    };
-
+    //event functionality on the submit button, fires the Post and Add functions, clears the input field
     $("#submitButton").on("click", function(event){
         event.preventDefault();
         post();
+        $("form").trigger("reset");
     });
-
-
 
 })
